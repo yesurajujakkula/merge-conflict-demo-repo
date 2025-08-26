80f12e52df13840922d490fda08aac44fcc4d803
Raju2662 <jakkula.raju@atos.net>
2025-08-26 18:16:59 +0530

    commiting the merge confilts

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ app=Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def home():
+    # this is the code changes for the developer b
     list2 = [2837,873246,987431]
     list1 = [1,2,3,5,6,7]
+    # this is the for the developer1 code changes
+    list1 = [1,2,3,5,6,7]
     return "Hello World"
+
 
 
 if __name__=="__main__":
